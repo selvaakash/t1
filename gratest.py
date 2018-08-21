31bb53513c3a8a6825d1e5648c5a2809acf2a25f
selvaakash <42028531+selvaakash@users.noreply.github.com>
2018-08-21 11:46:42 +0530

    Create gratest.py

---
+++ gratest.py
@@ -0,0 +1,10 @@
+a=input("enter the first number")
+b=input("enter the second number")
+c=input("entert the third number")
+
+if((a>b)&(a>c)):
+     print("a is greatest")
+elif((b>a)&(b>c)):
+     print("b is greatest")
+else:
+     print("c is greatest")
